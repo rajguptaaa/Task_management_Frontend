e6dcd77a66d5862eb28f7a3dfb9c38d7fc87041b
rajguptaaa <kumaaraj11@gmail.com>
2025-02-17 23:02:53 +0530

    added vercel.json

--- src/components/taskList.jsx
+++ src/components/taskList.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./taskList.css";
+import "./tasklist.css";
 import PropTypes from "prop-types";
 
 const TaskList = ({ list, getData, filterObj, title }) => {
